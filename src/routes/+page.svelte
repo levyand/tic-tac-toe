a19402f83d47fb589164c79ab0516e9cd793dcc2
Andrew Levy <levyand@gmail.com>
2023-10-04 22:41:26 -0500

    Add game component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
+<script lang="ts">
+  import GameBoard from "$lib/game-board.svelte";
+</script>
+
 <svelte:head>
   <title>Tic Tac Toe</title>
 </svelte:head>
 
 <h1>Tic Tac Toe</h1>
+
+<GameBoard />
